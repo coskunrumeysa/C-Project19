6838e25d2061fbaef34dc384e09af8882e08061d
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-06-01 09:51:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number1;
+            number1 = 123;
+
+            Console.WriteLine("Your byte number :" + " " + number1);
+            Console.Read();
+        }
+    }
+}
